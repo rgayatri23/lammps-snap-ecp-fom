b3f80f8a43f1a151a490467aba0204eacb6206dc
Rahulkumar Gayatri <rgayatri@lbl.gov>
2023-05-23 18:07:22 -0700

    Update num_atoms and num_steps for container run.

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-lmp -k on g 1 -sf kk -pk kokkos  newton on neigh half -in in.snap.test -var nsteps 1 -var nx 10 -var ny 10 -var nz 10 -var snapdir 2J14_W.SNAP
+lmp -k on g 4 -sf kk -pk kokkos  newton on neigh half -in in.snap.test -var nsteps 100 -var nx 100 -var ny 100 -var nz 10 -var snapdir 2J14_W.SNAP
